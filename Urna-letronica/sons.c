8b3a00cd804e5d1d32997de7aaaf8c6b66823bfc
Your Namecls <WesleyMAraujo>
2023-01-30 21:04:43 -0300

    ai

---
+++ Urna-letronica/sons.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
+#include <string.h>
+
+void som1()
+{
+    Beep(1881.8, 50);
+}
+
+void som2()
+{
+    Beep(1881.8, 50);
+    Beep(2089, 50);
+    Beep(1881.8, 50);
+    Beep(2089, 50);
+    Beep(1881.8, 50);
+    Beep(2089, 50);
+    Beep(1881.8, 50);
+    Beep(2089, 50);
+}
